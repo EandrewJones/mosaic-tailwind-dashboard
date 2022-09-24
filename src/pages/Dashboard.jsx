b1c2f8cc89f30bf32dabd93f1fb60d968d3b6017
Evan Jones <evan.a.jones3@gmail.com>
2022-09-23 22:23:14 -0400

    rename components; tailor to Oogway

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,108 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Sidebar from '../partials/Sidebar';
-import Header from '../partials/Header';
-import WelcomeBanner from '../partials/dashboard/WelcomeBanner';
-import DashboardAvatars from '../partials/dashboard/DashboardAvatars';
-import FilterButton from '../partials/actions/FilterButton';
-import Datepicker from '../partials/actions/Datepicker';
-import DashboardCard01 from '../partials/dashboard/DashboardCard01';
-import DashboardCard02 from '../partials/dashboard/DashboardCard02';
-import DashboardCard03 from '../partials/dashboard/DashboardCard03';
-import DashboardCard04 from '../partials/dashboard/DashboardCard04';
-import DashboardCard05 from '../partials/dashboard/DashboardCard05';
-import DashboardCard06 from '../partials/dashboard/DashboardCard06';
-import DashboardCard07 from '../partials/dashboard/DashboardCard07';
-import DashboardCard08 from '../partials/dashboard/DashboardCard08';
-import DashboardCard09 from '../partials/dashboard/DashboardCard09';
-import DashboardCard10 from '../partials/dashboard/DashboardCard10';
-import DashboardCard11 from '../partials/dashboard/DashboardCard11';
-import DashboardCard12 from '../partials/dashboard/DashboardCard12';
-import DashboardCard13 from '../partials/dashboard/DashboardCard13';
-import Banner from '../partials/Banner';
+import Sidebar from "../partials/Sidebar";
+import Header from "../partials/Header";
+import WelcomeBanner from "../partials/dashboard/WelcomeBanner";
+import DashboardAvatars from "../partials/dashboard/DashboardAvatars";
+import FilterButton from "../partials/actions/FilterButton";
+import Datepicker from "../partials/actions/Datepicker";
+import AreaChart01 from "../partials/dashboard/AreaChart01";
+import AreaChart02 from "../partials/dashboard/AreaChart02";
+import AreaChart03 from "../partials/dashboard/AreaChart03";
+import SideBySideBarChart from "../partials/dashboard/SideBySideBarChart";
+import RealTimeAreaChart from "../partials/dashboard/RealTimeAreaChart";
+import PieChart from "../partials/dashboard/PieChart";
+import DataTable01 from "../partials/dashboard/DataTable01";
+import MultiseriesLineChart from "../partials/dashboard/MultiseriesLineChart";
+import OverUnderBarChart from "../partials/dashboard/OverUnderBarChart";
+import DataTable02 from "../partials/dashboard/DataTable02";
+import HorizontalPieChart from "../partials/dashboard/HorizontalPieChart";
+import ActivityFeed from "../partials/dashboard/ActivityFeed";
+import IncomeExpenseFeed from "../partials/dashboard/IncomeExpenseFeed";
 
 function Dashboard() {
-
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex h-screen overflow-hidden">
-
+    <div className='flex h-screen overflow-hidden'>
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Content area */}
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-
+      <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
         <main>
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-
+          <div className='px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto'>
             {/* Welcome banner */}
             <WelcomeBanner />
 
             {/* Dashboard actions */}
-            <div className="sm:flex sm:justify-between sm:items-center mb-8">
-
+            <div className='sm:flex sm:justify-between sm:items-center mb-8'>
               {/* Left: Avatars */}
               <DashboardAvatars />
 
               {/* Right: Actions */}
-              <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+              <div className='grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2'>
                 {/* Filter button */}
                 <FilterButton />
                 {/* Datepicker built with flatpickr */}
                 <Datepicker />
                 {/* Add view button */}
-                <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
-                    <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
-                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                    </svg>
-                    <span className="hidden xs:block ml-2">Add view</span>
-                </button>                
+                <button className='btn bg-indigo-500 hover:bg-indigo-600 text-white'>
+                  <svg
+                    className='w-4 h-4 fill-current opacity-50 shrink-0'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z' />
+                  </svg>
+                  <span className='hidden xs:block ml-2'>Add view</span>
+                </button>
               </div>
-
             </div>
 
             {/* Cards */}
-            <div className="grid grid-cols-12 gap-6">
-
+            <div className='grid grid-cols-12 gap-6'>
               {/* Line chart (Acme Plus) */}
-              <DashboardCard01 />
+              <AreaChart01 />
               {/* Line chart (Acme Advanced) */}
-              <DashboardCard02 />
+              <AreaChart02 />
               {/* Line chart (Acme Professional) */}
-              <DashboardCard03 />
+              <AreaChart03 />
               {/* Bar chart (Direct vs Indirect) */}
-              <DashboardCard04 />
+              <SideBySideBarChart />
               {/* Line chart (Real Time Value) */}
-              <DashboardCard05 />
+              <RealTimeAreaChart />
               {/* Doughnut chart (Top Countries) */}
-              <DashboardCard06 />
+              <PieChart />
               {/* Table (Top Channels) */}
-              <DashboardCard07 />
+              <DataTable01 />
               {/* Line chart (Sales Over Time) */}
-              <DashboardCard08 />
+              <MultiseriesLineChart />
               {/* Stacked bar chart (Sales VS Refunds) */}
-              <DashboardCard09 />
+              <OverUnderBarChart />
               {/* Card (Customers) */}
-              <DashboardCard10 />
+              <DataTable02 />
               {/* Card (Reasons for Refunds) */}
-              <DashboardCard11 />
+              <HorizontalPieChart />
               {/* Card (Recent Activity) */}
-              <DashboardCard12 />
+              <ActivityFeed />
               {/* Card (Income/Expenses) */}
-              <DashboardCard13 />
-              
+              <IncomeExpenseFeed />
             </div>
-
           </div>
         </main>
-
-        <Banner />
-
       </div>
     </div>
   );
--- src/partials/dashboard/SideBySideBarChart.jsx
+++ src/partials/dashboard/SideBySideBarChart.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import BarChart from '../../charts/BarChart01';
+import React from "react";
+import BarChart from "../../charts/BarChart01";
 
 // Import utilities
-import { tailwindConfig } from '../../utils/Utils';
+import { tailwindConfig } from "../../utils/Utils";
 
-function DashboardCard04() {
-
+function SideBySideBarChart() {
   const chartData = {
     labels: [
-      '12-01-2020', '01-01-2021', '02-01-2021',
-      '03-01-2021', '04-01-2021', '05-01-2021',
+      "12-01-2020",
+      "01-01-2021",
+      "02-01-2021",
+      "03-01-2021",
+      "04-01-2021",
+      "05-01-2021",
     ],
     datasets: [
       // Light blue bars
       {
-        label: 'Direct',
-        data: [
-          800, 1600, 900, 1300, 1950, 1700,
-        ],
+        label: "Direct",
+        data: [800, 1600, 900, 1300, 1950, 1700],
         backgroundColor: tailwindConfig().theme.colors.blue[400],
         hoverBackgroundColor: tailwindConfig().theme.colors.blue[500],
         barPercentage: 0.66,
@@ -25,10 +26,8 @@ function DashboardCard04() {
       },
       // Blue bars
       {
-        label: 'Indirect',
-        data: [
-          4900, 2600, 5350, 4800, 5200, 4800,
-        ],
+        label: "Indirect",
+        data: [4900, 2600, 5350, 4800, 5200, 4800],
         backgroundColor: tailwindConfig().theme.colors.indigo[500],
         hoverBackgroundColor: tailwindConfig().theme.colors.indigo[600],
         barPercentage: 0.66,
@@ -38,9 +37,9 @@ function DashboardCard04() {
   };
 
   return (
-    <div className="flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
-      <header className="px-5 py-4 border-b border-slate-100">
-        <h2 className="font-semibold text-slate-800">Direct VS Indirect</h2>
+    <div className='flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200'>
+      <header className='px-5 py-4 border-b border-slate-100'>
+        <h2 className='font-semibold text-slate-800'>Direct VS Indirect</h2>
       </header>
       {/* Chart built with Chart.js 3 */}
       {/* Change the height attribute to adjust the chart height */}
@@ -49,4 +48,4 @@ function DashboardCard04() {
   );
 }
 
-export default DashboardCard04;
+export default SideBySideBarChart;
